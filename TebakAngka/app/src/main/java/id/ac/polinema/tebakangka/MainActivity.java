179f582b1165ca35b09351f2fd8a45c33ef53c0e
arkaleonant <arkaarif69@gmail.com>
2019-09-08 13:25:45 +0700

    Perbaikan Game Tebak Angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,24 +13,68 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	int angkaRandom = 0;
+	Button btn ;
+	Button rst;
+	EditText editAngka;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		btn = (Button) findViewById(R.id.guess_button);
+		rst = (Button) findViewById(R.id.reset_button);
+		editAngka = (EditText) findViewById(R.id.number_input);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (!editAngka.equals("")) {
+					handleGuess(view);
+				}
+			}
+		});
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
-
+	private int random(int angkaMin, int angkaMax){
+		Random rand = new Random();
+		int randomNum = rand.nextInt(angkaMax - angkaMin) + angkaMin;
+		return randomNum;
 	}
 
-	public void handleGuess(View view) {
+	private void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		int angkaTebakan = Integer.parseInt(editAngka.getText().toString());
+		if (angkaTebakan == angkaRandom){
+			Toast.makeText(this, "Tebakan Anda Benar", Toast.LENGTH_SHORT).show();
+		} else if (angkaTebakan > angkaRandom) {
+			Toast.makeText(this, "Tebakan Anda Terlalu Besar !", Toast.LENGTH_SHORT).show();
+		} else if (angkaTebakan < angkaRandom){
+			Toast.makeText(this, "Tebakan Anda Terlalu Kecil !", Toast.LENGTH_SHORT).show();
+		}
+		angkaRandom = random(1, 100);
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		rst.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view) {
+				editAngka.setText("");
+			}
+		});
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Toast.makeText(this, "Application Closed", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Toast.makeText(this, "Application Start", Toast.LENGTH_SHORT).show();
 	}
 }
